e6e55305432d4de7ff712ee6fe9c968ed7718088
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2023-03-19 01:57:55 -0700

    Update lungmask.py

--- lungmask.py
+++ lungmask.py
@@ -1,1 +1,77 @@
 
+from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
+from skimage.measure import label,regionprops, perimeter
+from skimage.morphology import binary_dilation, binary_opening
+from skimage.filters import roberts, sobel
+from skimage import measure, feature
+from skimage.segmentation import clear_border, mark_boundaries
+from skimage import data
+from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
+
+
+def get_segmented_lungs(raw_im, plot=False):        
+    # Borrowed from https://www.kaggle.com/code/kmader/dsb-lung-segmentation-algorithm | threshold updated to work with compressed images (max intensity: 255)       
+
+    im=raw_im.copy()
+    if plot == True:
+        f, plots = plt.subplots(8, 1, figsize=(5, 40))
+
+    binary = im < 100  # <--- updated here
+    if plot == True:
+        plots[0].axis('off')
+        plots[0].imshow(binary, cmap=plt.cm.bone) 
+
+    # Step 2: Remove the blobs connected to the border of the image        
+    cleared = clear_border(binary)
+    if plot == True:
+        plots[1].axis('off')
+        plots[1].imshow(cleared, cmap=plt.cm.bone) 
+
+    label_image = label(cleared)
+    if plot == True:
+        plots[2].axis('off')
+        plots[2].imshow(label_image, cmap=plt.cm.bone) 
+
+    # Step 4: Keep the labels with 2 largest areas        
+    areas = [r.area for r in regionprops(label_image)]
+    areas.sort()
+    if len(areas) > 2:
+        for region in regionprops(label_image):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:                
+                       label_image[coordinates[0], coordinates[1]] = 0
+    binary = label_image > 0
+    if plot == True:
+        plots[3].axis('off')
+        plots[3].imshow(binary, cmap=plt.cm.bone) 
+
+    # Step 5: Erosion operation with a disk of radius 2. This operation is 
+    # seperate the lung nodules attached to the blood vessels.        
+    selem = disk(2)
+    binary = binary_erosion(binary, selem)
+    if plot == True:
+        plots[4].axis('off')
+        plots[4].imshow(binary, cmap=plt.cm.bone) 
+
+    # Step 6: Closure operation with a disk of radius 10. This operation is 
+    # to keep nodules attached to the lung wall.        
+    selem = disk(10)
+    binary = binary_closing(binary, selem)
+    if plot == True:
+        plots[5].axis('off')
+        plots[5].imshow(binary, cmap=plt.cm.bone) 
+
+    # Step 7: Fill in the small holes inside the binary mask of lungs.        
+    edges = roberts(binary)
+    binary = ndi.binary_fill_holes(edges)
+    if plot == True:
+        plots[6].axis('off')
+        plots[6].imshow(binary, cmap=plt.cm.bone) 
+
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    if plot == True:
+        plots[7].axis('off')
+        plots[7].imshow(im, cmap=plt.cm.bone) 
+    return im, binary
